35b924773011b83f9e016c679ae3f5d858181436
Álvaro Gutiérrez <alvarogtrzcliment@gmail.com>
2024-03-01 17:23:09 +0100

    Webinar 1-03

--- ArcGIS-API-JS/ArcGIS-API-JS-3/function.js
+++ ArcGIS-API-JS/ArcGIS-API-JS-3/function.js
@@ -6,134 +6,128 @@ require([
   "esri/Color",
   "esri/dijit/PopupTemplate",
   "esri/tasks/query",
-  "esri/toolbars/draw",
   "esri/symbols/PictureMarkerSymbol",
   "esri/graphic",
-
-  "dojo/domReady!",
-], function (
-  Map,
-  FeatureLayer,
-  UniqueValueRenderer,
-  SimpleFillSymbol,
-  Color,
-  PopupTemplate,
-  Query,
-  Draw,
-  PictureMarkerSymbol,
-  Graphic
+  "esri/toolbars/draw",
   
-) {
-  let mapa = new Map("viewDiv", {
-    basemap: "gray-vector",
-    center: [-3.5, 40.4],
-    zoom: 5,
-  });
+  
+  "dojo/domReady!"],
+  
+  function(
+    Map,
+    FeatureLayer,
+    UniqueValueRenderer,
+    SimpleFillSymbol,
+    Color,
+    PopupTemplate,
+    Query,
+    PictureMarkerSymbol,
+    Graphic,
+    Draw){
 
-  let redNaturaFL = new FeatureLayer(
-    "https://services1.arcgis.com/nCKYwcSONQTkPA4K/arcgis/rest/services/Red_Natura_2000/FeatureServer/0"
-  );
+  let mapa = new Map("viewDiv",{
+    basemap:"gray-vector",
+    center:[-3.5,40.4],
+    zoom:5
+  })
 
-  let renderizadorRedNatura = new UniqueValueRenderer(redNaturaFL, "Tipo");
+  let redNaturaFL = new FeatureLayer('https://services1.arcgis.com/nCKYwcSONQTkPA4K/ArcGIS/rest/services/Red_Natura_2000/FeatureServer/0')
 
-  renderizadorRedNatura.addValue(
-    "LIC",
-    new SimpleFillSymbol().setColor(new Color("#cbf3f0"))
-  );
-  renderizadorRedNatura.addValue(
-    "ZEPA",
-    new SimpleFillSymbol().setColor(new Color("#ffbf69"))
-  );
+  let renderizadorRedNatura = new UniqueValueRenderer(redNaturaFL,'Tipo')
 
-  redNaturaFL.renderer = renderizadorRedNatura;
+  renderizadorRedNatura.addValue('LIC', new SimpleFillSymbol().setColor(new Color('#cbf3f0')))
 
-  let playasFl = new FeatureLayer(
-    "https://services1.arcgis.com/nCKYwcSONQTkPA4K/arcgis/rest/services/Playas_2015/FeatureServer/0",{
-      outFields:['Nombre','Descripci','Longitud','Anchura','Condicione','Submarinis']
-    }
-    
-  );
+  renderizadorRedNatura.addValue('ZEPA', new SimpleFillSymbol().setColor(new Color('#ffbf69')))
+
+  redNaturaFL.renderer = renderizadorRedNatura
+
+  let playasFL = new FeatureLayer('https://services1.arcgis.com/nCKYwcSONQTkPA4K/ArcGIS/rest/services/Playas_2015/FeatureServer/0',{
+    outFields:['Nombre','Descripci','Longitud','Anchura','Condicione','Submarinis']
+  })
 
   let plantillaPopup = new PopupTemplate({
-    title: '{Nombre}',
-    fieldInfos: [
-      {
-        fieldName: "Descripci",
-        visible:true,
-        label: "Descripción de la playa",
-      },
-      {
-        fieldName: "Longitud",
-        visible:true,
-        label: "Longitud de la playa",
-      },
-      {
-        fieldName: "Anchura",
-        visible:true,
-        label: "Anchura de la playa",
-      },
-      {
-        fieldName: "Condicione",
-        visible:true,
-        label: "Condiciones de la playa",
-      },
-    ],
-  });
+    title:'{Nombre}',
+    fieldInfos:[{
 
-  playasFl.infoTemplate = plantillaPopup;
+      fieldName:'Descripci',
+      visible:true,
+      label:'Descripción de la Playa'
+
+    },{
+
+      fieldName:'Longitud',
+      visible:true,
+      label:'Longitud de la Playa'
+
+    },{
+
+      fieldName:'Anchura',
+      visible:true,
+      label:'Anchura de la Playa'
+
+    },{
+      fieldName:'Condicione',
+      visible:true,
+      label:'Condiciones de la Playa'
+    }]
+  })
+
+  playasFL.infoTemplate = plantillaPopup
 
   mapa.on('load',()=>{
-    let playasSubmarinismoQuery = new Query()
 
-    playasSubmarinismoQuery.where = "Submarinis = 'Sí'"
+    let playasSubarinismoQuery = new Query()
+    
+    playasSubarinismoQuery.where = "Submarinis = 'Sí'"
 
-    playasFl.queryFeatures(playasSubmarinismoQuery,(featureSet)=>{
-      featureSet.features.map((playa)=>{
+    playasFL.queryFeatures(playasSubarinismoQuery,(featureSet)=>{
+      
+      featureSet.features.map((playa) => {
 
         let simbologiaPlaya = new PictureMarkerSymbol('https://cdn-icons-png.flaticon.com/512/3145/3145021.png',20,20)
         let graficoPlaya = new Graphic(playa.geometry,simbologiaPlaya)
         mapa.graphics.add(graficoPlaya)
 
       })
+
     })
-  
+
   })
 
   mapa.on('load',innitToolbar)
+
   let toolbar = new Draw(mapa)
 
-  // Inicializar la herramienta de dibujo para pintar polígonos
-
   function innitToolbar(){
-      toolbar.on('draw-end',addGraphic)
-      toolbar.activate(Draw.POLYGON)
-      
+    toolbar.on('draw-end',addGraphic)
+    toolbar.activate(Draw.POLYGON)
   }
 
-  // Mostrar el polígono dibujado
-  
-  function addGraphic(evt){
-      toolbar.deactivate()
-      mapa.enableMapNavigation();
-      let poligono = new SimpleFillSymbol()
-      let symbol = new Graphic(evt.geometry,poligono)
-      mapa.graphics.add(symbol)
-      selectEarthquakes(evt.geometry)
+  function addGraphic(evento){
+    toolbar.deactivate()
+    mapa.enableMapNavigation()
+
+    let simbologiaPoligono = new SimpleFillSymbol()
+    let graficoPoligono = new Graphic(evento.geometry,simbologiaPoligono)
+
+    mapa.graphics.add(graficoPoligono)
+
+    selectRedNatura2000(evento.geometry)
+  }
+
+  function selectRedNatura2000(inputGeometry){
+
+    let consultaRedNatura = new Query()
+    consultaRedNatura.geometry = inputGeometry
+
+    redNaturaFL.selectFeatures(consultaRedNatura)
+    let simbologiaPoligonoRedNatura = new SimpleFillSymbol
+    simbologiaPoligonoRedNatura.setColor([255,0,0,1])
+    redNaturaFL.setSelectionSymbol(simbologiaPoligonoRedNatura)
 
   }
 
-  function selectEarthquakes(inputGeometria){
-      let consulta = new Query()
-      consulta.geometry = inputGeometria
-      redNaturaFL.selectFeatures(consulta)
-      let poligonoSymbolo = new SimpleFillSymbol()
-      poligonoSymbolo.setColor(new Color([255, 0, 0, 1]));
-      redNaturaFL.setSelectionSymbol(poligonoSymbolo)
-  }
-
-  
+  mapa.addLayers([redNaturaFL,playasFL])
 
 
-
-  mapa.addLayers([redNaturaFL, playasFl]);
-});
+})
